3b49faf82e8bb24b809d45d980913b30262511e3
d-smith <doug.smith.mail@gmail.com>
2019-01-31 10:27:59 -0800

    Added random failures and task retry

--- src/datamunger.js
+++ src/datamunger.js
@@ -2,10 +2,19 @@ const aws = require('aws-sdk');
 const s3 = new aws.S3();
 const s3utils = require('./s3utils');
 
+var Chance = require('chance');
+
+// Instantiate Chance so it can be used
+var chance = new Chance();
 
 exports.munge = async (event, context) => {
     console.log(`munge called with event ${JSON.stringify(event)}`);
     console.log(`source bucket is ${event.Bucket}, key ${event.Key}`);
+
+    //Simulate an error
+    if(chance.bool({ likelihood: 55 })) {
+        throw new Error("boom");
+    }
 
     let data = await s3utils.readInputDataJSON(s3, event.Bucket, event.Key, (x)=> true);
     console.log(`input data is ${data}`);
